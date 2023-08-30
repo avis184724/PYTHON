3fde046ed8cc12440b7e69f95fda020ca72c4931
avis184724 <134455536+avis184724@users.noreply.github.com>
2023-08-30 09:11:56 +0900

    FullName 코드
    성과 이름을 입력 받은 뒤 합쳐서 출력하는 간단한 프로그램

---
+++ fullname.py
@@ -0,0 +1,5 @@
+last_name = input("성을 입력하세요 : ") # 성 입력 받기
+name = input("이름을 입력하세요 : ") # 이름 입력 받기
+
+Full_name = last_name + name # Full_name 이라는 변수에 성과 이름을 넣음
+print("Full_name : " + Full_name) # Full_name 출력
